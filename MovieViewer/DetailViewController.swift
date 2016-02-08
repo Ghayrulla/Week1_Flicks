4011a5545e6ae0245ce9fe933bd25d57efd28b16
ghayrulla <ghayrullaghayrat@hotmail.com>
2016-02-08 18:21:43 -0500

    Week_2 Monday work completed

---
+++ MovieViewer/DetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DetailViewController.swift
+//  MovieViewer
+//
+//  Created by Gary Ghayrat on 2/7/16.
+//  Copyright © 2016 Gary Ghayrat. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var posterimageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    var movie: NSDictionary!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let title = movie["title"] as? String
+        titleLabel.text = title
+        
+        let overview = movie["overview"]
+        overviewLabel.text = overview as? String
+        // Do any additional setup after loading the view.
+        
+        let baseUrl = "http://image.tmdb.org/t/p/w500"
+        
+        if let posterPath = movie["poster_path"] as? String {
+            let imageUrl = NSURL(string: baseUrl + posterPath)
+            posterimageView.setImageWithURL(imageUrl!)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
